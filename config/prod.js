aad5e293e784ee8ad25c5c982788574565b8a417
amor <amoriqbalmail@gmail.com>
2020-06-30 09:29:39 +0530

    added prod.js

---
+++ config/prod.js
@@ -0,0 +1,7 @@
+module.exports={
+    mongodbURI:process.env.MONGODB_URI,
+    port:process.env.PORT,
+    sessionKey:process.env.SESSION_KEY,
+    googleClientId:process.env.GOOGLE_CLIENT_ID,
+    googleClientSecret:process.env.GOOGLE_CLIENT_SECRET
+}
